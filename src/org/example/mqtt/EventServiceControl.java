82ec49e0542a12cb1b1392939b4c73df3e3c5812
Pablo Marcondes Fonseca <pablo.mmarcondes@gmail.com>
2012-11-20 23:11:56 -0200

    versao com messaging

--- src/org/example/mqtt/EventServiceControl.java
+++ src/org/example/mqtt/EventServiceControl.java
@@ -11,63 +11,153 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class EventServiceControl extends Activity {
 
-	private IntentFilter intentFilter;
-	private EventService serviceBinder;
-	Intent eventServiceIntent;
+	/** Messenger for communicating with service. */
+    Messenger mService = null;
+    /** Flag indicating whether we have called bind on the service. */
+    boolean mIsBound;
+    /** Some text view we are using to show state information. */
+    TextView mCallbackText;
+    
+    /**
+     * Handler of incoming messages from service.
+     */
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case EventService.MSG_SET_VALUE:
+                    mCallbackText.setText("Received from service: " + msg.arg1);
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+    
+    /**
+     * Target we publish for clients to send messages to IncomingHandler.
+     */
+    final Messenger mMessenger = new Messenger(new IncomingHandler());
+
+    /**
+     * Class for interacting with the main interface of the service.
+     */
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className,
+                IBinder service) {
+            // This is called when the connection with the service has been
+            // established, giving us the service object we can use to
+            // interact with the service.  We are communicating with our
+            // service through an IDL interface, so get a client-side
+            // representation of that from the raw service object.
+            mService = new Messenger(service);
+            mCallbackText.setText("Attached.");
+
+            // We want to monitor the service for as long as we are
+            // connected to it.
+            try {
+                Message msg = Message.obtain(null,
+                        EventService.MSG_REGISTER_CLIENT);
+                msg.replyTo = mMessenger;
+                mService.send(msg);
+                
+                // Give it some value as an example.
+                msg = Message.obtain(null,
+                        EventService.MSG_SET_VALUE, this.hashCode(), 0);
+                mService.send(msg);
+            } catch (RemoteException e) {
+                // In this case the service has crashed before we could even
+                // do anything with it; we can count on soon being
+                // disconnected (and then reconnected if it can be restarted)
+                // so there is no need to do anything here.
+            }
+            
+            // As part of the sample, tell the user what happened.
+            Toast.makeText(getBaseContext(), R.string.remote_service_connected,
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            // This is called when the connection with the service has been
+            // unexpectedly disconnected -- that is, its process crashed.
+            mService = null;
+            mCallbackText.setText("Disconnected.");
+
+            // As part of the sample, tell the user what happened.
+            Toast.makeText(getBaseContext(), R.string.remote_service_disconnected,
+                    Toast.LENGTH_SHORT).show();
+        }
+    };
+    
+    void doBindService() {
+        // Establish a connection with the service.  We use an explicit
+        // class name because there is no reason to be able to let other
+        // applications replace our component.
+        bindService(new Intent(this, 
+                EventService.class), mConnection, Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+        mCallbackText.setText("Binding.");
+    }
+    
+    void doUnbindService() {
+        if (mIsBound) {
+            // If we have received the service, and hence registered with
+            // it, then now is the time to unregister.
+            if (mService != null) {
+                try {
+                    Message msg = Message.obtain(null,
+                            EventService.MSG_UNREGISTER_CLIENT);
+                    msg.replyTo = mMessenger;
+                    mService.send(msg);
+                } catch (RemoteException e) {
+                    // There is nothing special we need to do if the service
+                    // has crashed.
+                }
+            }
+            
+            // Detach our existing connection.
+            unbindService(mConnection);
+            mIsBound = false;
+            mCallbackText.setText("Unbinding.");
+        }
+    }
+
+
+    
 	
-	private ServiceConnection connection = new ServiceConnection() {
-		
-		public void onServiceDisconnected(ComponentName name) {
-			serviceBinder = null;
-		}
-		
-		public void onServiceConnected(ComponentName name, IBinder service) {
-			serviceBinder = ((EventService.EventServiceBinder)service).getService();
-			try {
-			URL[] urls = new URL[] {
-					new URL("http://www.e1.com/f1")
-			};
-			serviceBinder.urls = urls;
-			} catch (MalformedURLException e) {
-				e.printStackTrace();
-			}
-			startService(eventServiceIntent);
-		}
-	};
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.event_control);
 		
-		intentFilter = new IntentFilter();
-		intentFilter.addAction("FILE_DOWNLOADED_ACTION");
-		
-		registerReceiver(intentReceiver, intentFilter);
-		
-		
-		
 		Button btnStart = (Button) findViewById(R.id.btnStartService);
 		btnStart.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				eventServiceIntent = new Intent(EventServiceControl.this, EventService.class);
-				bindService(eventServiceIntent, connection, Context.BIND_AUTO_CREATE);
+				doBindService();
 			}
 		});
 		Button btnStop = (Button) findViewById(R.id.btnStopService);
 		btnStop.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				stopService(new Intent(getBaseContext(), EventService.class));
+				doUnbindService();
 			}
 		});
 
+		mCallbackText = (TextView)findViewById(R.id.callback);
+        mCallbackText.setText("Not attached.");
 	}
 	
 	private BroadcastReceiver intentReceiver = new BroadcastReceiver() {
@@ -81,7 +171,7 @@ public class EventServiceControl extends Activity {
 
 	@Override
 	protected void onDestroy() {
-		unregisterReceiver(intentReceiver);
+		//unregisterReceiver(intentReceiver);
 		super.onDestroy();
 	}
 
